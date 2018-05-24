a67f862491806419025a357413f5d0196e56dba9
MomNoodle <ss313022298@163.com>
2018-05-24 15:02:14 +0800

    asd

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+import markdown
+from django.utils.html import strip_tags
+
 
 # Create your models here.
 # 创建分类
@@ -23,6 +26,23 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     # 摘要
     excerpt = models.CharField(max_length=200, blank=True)
+
+    def save(self, *args, **kwargs):
+        # 如果没有填写摘要
+        if not self.excerpt:
+            # 首先实例化一个 Markdown 类，用于渲染 body 的文本
+            md = markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+            ])
+            # 先将 Markdown 文本渲染成 HTML 文本
+            # strip_tags 去掉 HTML 文本的全部 HTML 标签
+            # 从文本摘取前 54 个字符赋给 excerpt
+            self.excerpt = strip_tags(md.convert(self.body))[:54]
+
+        # 调用父类的 save 方法将数据保存到数据库中
+        super(Post, self).save(*args, **kwargs)
+
     # 正文
     body = models.TextField()
     # 创建时间
